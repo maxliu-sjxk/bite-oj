8e9156db7835c8f98de1bcb0d4f27a91f1a90cfc
si-jiao <3108772934@qq.com>
2025-08-17 21:18:37 +0800

    拉取mysql镜像，配置相关信息，确保项目启动并同时查看swagger生成的文档

--- oj-modules/oj-system/src/main/java/com/bite/system/OjSystemApplication.java
+++ oj-modules/oj-system/src/main/java/com/bite/system/OjSystemApplication.java
@@ -4,6 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * localhost:9201/swagger-ui/index.html
+ */
 @MapperScan("com.bite.system.mapper")
 @SpringBootApplication
 public class OjSystemApplication {
