cb5edf3a8c4c52702ea08643f60ae103f4a7a27e
si-jiao <3108772934@qq.com>
2025-08-18 14:28:09 +0800

    全局异常处理器、日志框架

---
+++ oj-common/oj-common-security/src/main/java/com/bite/common/security/GlobalExceptionHandler.java
@@ -0,0 +1,55 @@
+package com.bite.common.security;
+
+import com.bite.common.core.domain.R;
+import com.bite.common.core.enums.ResultCode;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * 请求方法不支持
+     * @param e
+     * @param request
+     * @return
+     */
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public R<?> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e,
+                                                             HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+        log.error("请求地址'{}', 不支持'{}'请求", requestURI, e.getMethod());
+        return R.fail(ResultCode.ERROR);
+    }
+
+
+    /**
+     * 运行时异常
+     * @param e
+     * @param request
+     * @return
+     */
+    @ExceptionHandler(RuntimeException.class)
+    public R<?> handleRuntimeException(RuntimeException e, HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+        log.error("请求地址'{}', 发生异常.", requestURI, e);
+        return R.fail(ResultCode.ERROR);
+    }
+
+    /**
+     * 全局异常兜底
+     * @param e
+     * @param request
+     * @return
+     */
+    @ExceptionHandler(Exception.class)
+    public R<?> handleException(Exception e, HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+        log.error("请求地址'{}', 发生异常.", requestURI, e);
+        return R.fail(ResultCode.ERROR);
+    }
+}
