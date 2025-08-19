c4b7b07ae3687675e71d795ccbb0c4a0e352d297
si-jiao <3108772934@qq.com>
2025-08-19 13:56:09 +0800

    确定身份验证方案：JWT+Redis，引入Redis

---
+++ oj-modules/oj-system/src/main/java/com/bite/system/test/controller/TestController.java
@@ -0,0 +1,33 @@
+package com.bite.system.test.controller;
+
+import com.bite.common.redis.service.RedisService;
+import com.bite.system.domain.SysUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @Autowired
+    private RedisService redisService;
+
+    @RequestMapping("/redis")
+    public String testRedis() {
+        redisService.setCacheObject("testString", "String");
+        String testString = redisService.getCacheObject("testString", String.class);
+        System.out.println(testString);
+
+        redisService.setCacheObject("testInteger", 1);
+        Integer testInteger = redisService.getCacheObject("testInteger", Integer.class);
+        System.out.println(testInteger);
+
+        redisService.setCacheObject("testObject", new SysUser());
+        SysUser user = redisService.getCacheObject("testObject", SysUser.class);
+        System.out.println(user);
+
+        System.out.println();
+        return "test";
+    }
+}
