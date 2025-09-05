531d0754685691c33e216857991c2870967a0497
si-jiao <3108772934@qq.com>
2025-09-05 09:06:22 +0800

    validation后端参数校验

--- oj-common/oj-common-security/src/main/java/com/bite/common/security/handler/GlobalExceptionHandler.java
+++ oj-common/oj-common-security/src/main/java/com/bite/common/security/handler/GlobalExceptionHandler.java
@@ -1,13 +1,25 @@
 package com.bite.common.security.handler;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.StrUtil;
 import com.bite.common.core.domain.R;
 import com.bite.common.core.enums.ResultCode;
 import com.bite.common.security.exception.ServiceException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Collection;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice
@@ -35,6 +47,23 @@ public class GlobalExceptionHandler {
         return R.fail(resultCode);
     }
 
+    //TODO 是否需要添加 ConstraintViolationException 异常处理器
+
+    @ExceptionHandler(BindException.class)
+    public R<Void> handleBindException(BindException e) {
+        log.error(e.getMessage());
+        String message = join(e.getAllErrors(),
+                DefaultMessageSourceResolvable::getDefaultMessage, ", ");
+        return R.fail(ResultCode.FAILED_PARAMS_VALIDATE.getCode(), message);
+    }
+
+    private <E> String join(Collection<E> collection, Function<E, String>
+            function, CharSequence delimiter) {
+        if (CollUtil.isEmpty(collection)) {
+            return StrUtil.EMPTY;
+        }
+        return collection.stream().map(function).filter(Objects::nonNull).collect(Collectors.joining(delimiter));
+    }
 
     /**
      * 运行时异常
